cf259315209dfa1fb8082470da7dab055064ab71
Quinn Lashinsky <quinnlashinsky@wework.com>
2018-03-07 21:30:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function theBeatlesPlay(musicians, instruments) {
+  var beatlesArray = [];
+  musicians;
+  for (var i = 0; i < musicians.length; i++) {
+    beatlesArray.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return beatlesArray;
+}
+
+function johnLennonFacts(johnLennonFactsArray){
+ var factsArray = [];
+  var i = 0;
+while (i < johnLennonFactsArray.length) {
+   factsArray.push(johnLennonFactsArray[i] + "!!!");
+   i++;
+  }
+  return factsArray;
+}
+
+function iLoveTheBeatles(number) {
+  var iLoveTheBeatlesArray = [];
+  var i = 0;
+  do {iLoveTheBeatlesArray.push("I love the Beatles!");
+    
+  }
+   while(i < 16);
+   i++;
+}
